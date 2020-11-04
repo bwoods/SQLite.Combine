dcac345d0f57915fa7dca4f74dffa50c9370c264
Bryan Woods <woods.bryan@gmail.com>
2020-11-03 16:43:07 -0800

    SQLite.error is now public

--- Sources/SQLite.Combine/SQLite.swift
+++ Sources/SQLite.Combine/SQLite.swift
@@ -5,7 +5,7 @@ import SQLite3
 public final class SQLite {
 	public private(set) var pointer: OpaquePointer
 
-	static func error(from db: OpaquePointer, _ supplemental: String = "SQLite failed") -> Error {
+	static public func error(from db: OpaquePointer, _ supplemental: String = "SQLite failed") -> Error {
 		NSError(domain: "sqlite", code: numericCast(sqlite3_errcode(db)), userInfo: [
 			NSLocalizedFailureReasonErrorKey : String(cString: sqlite3_errmsg(db)),
 			NSLocalizedFailureErrorKey : supplemental
